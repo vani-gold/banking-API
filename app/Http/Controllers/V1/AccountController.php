49603f095c75707c446c5a1b5a1460e61c047b02
vani-gold <esibevan@yahoo.com>
2022-11-17 05:28:10 +0100

    implement the create account API

---
+++ app/Http/Controllers/V1/AccountController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Models\Account;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreAccountRequest;
+use App\Http\Requests\UpdateAccountRequest;
+
+class AccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Account::all();  //post
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAccountRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAccountRequest $request)
+    {
+        $account = Account::create($request->all());
+
+        return $account;  //get all
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Account  $account
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Account $account)
+    {
+        return $account; //get 1
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAccountRequest  $request
+     * @param  \App\Models\Account  $account
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAccountRequest $request, Account $account)
+    {
+        $account->update($request->all());
+
+        return $account;  
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Account  $account
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Account $account)
+    {
+        $account->delete();
+
+        return response('deleted', 204);
+    }
+}
---
+++ database/migrations/2022_11_16_171249_create_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('holder');
+            $table->string('location');
+            $table->decimal('initial_deposit', 64, 0)->default(50000);
+            $table->timestamps();
+            $table->foreignIdFor(\App\Models\User ::class, 'user_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
